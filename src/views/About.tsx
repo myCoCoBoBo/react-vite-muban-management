a152c0276e04ad509b5b42674622651ef8892e19
wujie <1475970138@qq.com>
2022-12-25 22:47:26 +0800

    实现首页跳转

---
+++ src/views/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      我是about组件anagementvscode 自带的终端不能粘贴PS D:\项目\react\vite-management> vscode自带的命令行不能粘贴le                                                                                                    
+    </div>
+  )
+}
