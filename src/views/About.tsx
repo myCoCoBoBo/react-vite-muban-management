54954055d0212920749e45fa8611e12867276f59
wujie <1475970138@qq.com>
2022-12-28 22:37:01 +0800

    修改

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      我是about组件anagementvscode 自带的终端不能粘贴PS D:\项目\react\vite-management> vscode自带的命令行不能粘贴le                                                                                                    
+      我是about组件anagementvscode 自带的终端不能粘贴PS D:\项目\react\vite-management vscode自带的命令行不能粘贴le                                                                                                    
     </div>
   )
 }
